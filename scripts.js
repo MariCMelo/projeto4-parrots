df715f575a497de6b9ac3fde6d3d76dde7ca3af6
marilia <marilia.correa.melo@gmail.com>
2023-01-12 23:39:25 -0300

    adicao de cartas

--- scripts.js
+++ scripts.js
@@ -3,5 +3,25 @@ const cartasArray = [
     {
      nome:'bobRoss',
      img:  'imagens/bobrossparrot.gif'  
-    }
+    },
+    {
+        nome:'bobRoss',
+     img:  'imagens/bobrossparrot.gif'  
+    },
+    {
+        nome:'explodyParrot',
+     img:  'imagens/explodyparrot.gif'  
+    },
+    {
+         nome:'explodyParrot',
+     img:  'imagens/explodyparrot.gif'  
+    },
+    {
+      nome:'fiestaParrot',
+     img:  'imagens/piestaparrot.gif'    
+    },
+    {nome:'fiestaParrot',
+     img:  'imagens/piestaparrot.gif' 
+    },
+
 ]
